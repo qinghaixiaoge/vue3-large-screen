d6d4d2684a92a0eb8cfdb26dbcd05c3b21ffc9e8
清海 <1308952908@qq.com>
2024-09-19 15:48:34 +0800

    主分支

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const TerserPlugin = require('terser-webpack-plugin');
 const cdn = ["https://cdn.bootcdn.net/ajax/libs/echarts/5.4.3/echarts.min.js"]
 const externals = {
   "echarts": "echarts",
@@ -16,6 +17,14 @@ module.exports = defineConfig({
           return args;
         }).end()
         .externals(externals)
+      config.optimization.minimize(true);
+      config.optimization.minimizer('terser').use(TerserPlugin, [{
+        terserOptions: {
+          compress: {
+            drop_console: true, // 删除所有 console 语句
+          },
+        },
+      }]);
     }
   }
 })
